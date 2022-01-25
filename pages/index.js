7b06a7674a2be8dfaf3ea314a3fbf5342c2781c0
Gustavo <valbuena52@gmail.com>
2022-01-25 15:37:16 -0300

    add toggle dark mode

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+      <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} mb={6} p={3} align="center">
         Hello, I&apos;m a Sofware engineer based in Buenos Aires
       </Box>
 
@@ -12,7 +12,24 @@ const Page = () => {
           <Heading as="h2" variant="page-title">
             Gustavo Valbuena
           </Heading>
-          <p>Software Engineer / Sound Explorer</p>
+          <p>Software Engineer / Sound Explorer / Digital Artist</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/gavu.jpg"
+            alt="Profile image"
+          />
         </Box>
       </Box>
     </Container>
